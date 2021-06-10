bf7301d9149ed8864a1fef5604e361ecc6db630b
devinhornj <horndj@plu.edu>
2021-06-09 21:44:18 -0700

    yes

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+root = Tk()
+
+img = Image.open("images\city.png")
+
+imgD = ImageTk.PhotoImage(img)
+
+label = tkinter.Label(image=imgD)
+label.image = imgD
+label.place(x=0,y=0)
+
+
+width, height = img.size
+size = str(width) + "x" + str(height + 50)
+
+button1 = Button(text="Good")
+button1.place(x=0, y=height+25)
+
+button1 = Button(text="Bad")
+button1.place(x=75, y=height+25)
+
+root.geometry(size)
+root.mainloop()
+
